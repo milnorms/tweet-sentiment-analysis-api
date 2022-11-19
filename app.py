2f953038d70587e97f21a93777f3223e3b47a86c
milnorms <minutemili@gmail.com>
2022-11-19 03:09:05 -1000

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Import function to generate tweet sentiment analysis data
+from tweetSentimentAnalysis import getTweetData
+# Import sanic for api endpoint and handling requests
+from sanic import Sanic
+from sanic.response import json
+# Import os for env variables
+import os
+
+# to run
+# 1. source env/bin/activate
+# 2. python app.py
+
+# Git venv guide: https://medium.com/wealthy-bytes/the-easiest-way-to-use-a-python-virtual-environment-with-git-401e07c39cde
+ 
+app = Sanic("app")
+ 
+async def getData(term, numItems, numWordCount):
+    '''
+    Async wrapper for getTweetData()
+    '''
+    return getTweetData(term, numItems, numWordCount)
+ 
+# webapp path defined used route decorator
+@app.route("/api")
+async def run(request):
+    # First query arg is search term, second is number of tweets to be returned, third is the number of word count words to be returned
+    # eg. http://0.0.0.0:8000/api?t=from:elonmusk&n=100&w=50
+    print(request.query_args)
+    term = request.query_args[0][-1]
+    numItems = int(request.query_args[1][-1])
+    numWordCount = int(request.query_args[2][-1])
+    data = await getData(term, numItems, numWordCount)
+    return json(data)
+ 
+
+# Running app with env variables for heroku deployment. Ref: https://blog.mayortech.co.uk/posts/running-a-sanic-app-on-heroku/
+app.run(
+    host='0.0.0.0',
+    port=int(os.environ.get('PORT', 8000)),
+    workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+    debug=bool(os.environ.get('DEBUG', True)),
+    auto_reload=True
+)
