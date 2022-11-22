5fe3198d3906f23e7785e218303d16f72ce54451
milnorms <minutemili@gmail.com>
2022-11-22 06:23:08 -1000

    updated keys

--- keys.py
+++ keys.py
@@ -5,9 +5,3 @@ from dotenv import load_dotenv
 #if you set .env in different directory, put the directory address load_dotenv("directory_of_.env)
 load_dotenv()
 
-
-    # CONSUMER_KEY=uMmUeZMov4k766jC0Ie0TRM46
-    # CONSUMER_SECRET=sVQ43l4BdMCGZ43uVdXpENDSbzreUjV52W4fQwu8WjhcxQuMAu
-    # ACCESS_TOKEN=2748024674-uN0GbEfzYpW414Bl5921hCQcblhfroOLDSyytms
-    # ACCESS_TOKEN_SECRET=QLBf5zZN2pcyuKEyXJ423v0rvMHBKkJys863ZF9tIDg5L
-
