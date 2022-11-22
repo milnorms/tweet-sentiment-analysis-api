51b4baf91a4ba342fd6990eb1825bf4139467398
milnorms <minutemili@gmail.com>
2022-11-22 06:19:19 -1000

    took keys off .gitignore

---
+++ keys.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv 
+
+#default directory for .env file is the current directory
+#if you set .env in different directory, put the directory address load_dotenv("directory_of_.env)
+load_dotenv()
+
+
+    # CONSUMER_KEY=uMmUeZMov4k766jC0Ie0TRM46
+    # CONSUMER_SECRET=sVQ43l4BdMCGZ43uVdXpENDSbzreUjV52W4fQwu8WjhcxQuMAu
+    # ACCESS_TOKEN=2748024674-uN0GbEfzYpW414Bl5921hCQcblhfroOLDSyytms
+    # ACCESS_TOKEN_SECRET=QLBf5zZN2pcyuKEyXJ423v0rvMHBKkJys863ZF9tIDg5L
+
