5927635aa9119ff4f069423dc32a542979ca3809
milnorms <minutemili@gmail.com>
2023-01-10 05:15:48 -1000

    added comments for debugging cors

--- cors.py
+++ cors.py
@@ -5,12 +5,12 @@ def _add_cors_headers(response, methods: Iterable[str], host) -> None:
     allow_methods = list(set(methods))
     if "OPTIONS" not in allow_methods:
         allow_methods.append("OPTIONS")
+    print(f'Provided HOST ===>>> {host}')
     # Allowed origins to access api. Local dev host and deployment host
     allow_origins = ["http://localhost:3000", "https://tweet-sentiment-webapp.vercel.app"]
     # If the host isnt found, set default access to local dev host
     if host not in allow_origins:
             host = "http://localhost:3000"
-
     print(f'ALLOWED HOST ===>>> {host}')
     headers = {
         "Access-Control-Allow-Methods": ",".join(allow_methods),
